070b0ef0f19c1cd101195a917e1fc5b2638ff90d
malakehab2003 <lokaehab2003@gmail.com>
2023-07-13 22:55:07 +0000

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	char op[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(op);
+
+	return (0);
+}
