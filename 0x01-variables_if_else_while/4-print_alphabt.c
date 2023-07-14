50d2ca36cd3867d5ebe822bd2baffddafad35391
malakehab2003 <lokaehab2003@gmail.com>
2023-07-14 17:47:08 +0000

    print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i = 0;
+
+	while (i < 26)
+	{
+		if (i == 16 || i == 4)
+		{
+			continue;
+		}
+		putchar('a' + i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
