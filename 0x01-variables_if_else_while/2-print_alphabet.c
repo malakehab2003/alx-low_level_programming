7111d7129de892dda6f213c043e65edfb5467903
malakehab2003 <lokaehab2003@gmail.com>
2023-07-14 17:16:08 +0000

    print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	for (int i = 0; i < 26; i++)
+	{
+		putchar('a' + i);
+		putchar('\n');
+	}
+	return (0);
+}
