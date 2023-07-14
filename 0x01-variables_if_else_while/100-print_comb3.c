29489dd79baa2d8f79d689a5217f8ba78a044131
malakehab2003 <lokaehab2003@gmail.com>
2023-07-14 21:03:17 +0000

    print_comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i = 0;
+
+	int j = 0;
+
+	while (i < 10)
+	{
+	j = 0;
+		while (j < 10)
+		{
+			if (i == j)
+			{
+				j++;
+				continue;
+			}
+			putchar('0' + i);
+			putchar('0' + j);
+			if (i < 9 || j < 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			j++;
+		}
+	i++;
+	}
+	putchar('\n');
+}
