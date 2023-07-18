8d1ecad6b83beb238f28c05e357d0fb326f21223
malakehab2003 <lokaehab2003@gmail.com>
2023-07-18 22:51:53 +0000

    time_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,22 +10,27 @@
 
 void times_table(void)
 {
-	int i = 0, j = 0;
+	int i = 0, j = 0, m;
 
 	while (i < 10)
 	{
 		j = 0;
 		while (j < 10)
 		{
-			_putchar('0' + (i * j));
-			if ((i + j) == 20)
+			m = i * j;
+			if (m > 9)
 			{
-				_putchar('$');
+				_putchar((m / 10) + '0');
+			}
+			_putchar('0' + m);
+			if (m == 18)
+			{
+				_putchar('\n');
 			}
 			else
 			{
 				_putchar(',');
-				if (((i * j) / 10) == 0)
+				if ((m / 10) == 0)
 				{
 					_putchar(' ');
 					_putchar(' ');
