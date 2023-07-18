24aa3d862a5cced36c86c47b876d5f4e6a8fca00
malakehab2003 <lokaehab2003@gmail.com>
2023-07-18 22:05:39 +0000

    print the last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -13,7 +13,14 @@
 int print_last_digit(int n)
 {
 	int l = (n % 10);
-
-	_putchar(l + '0');
+	
+	if (n >= 0)
+	{
+		_putchar(l + '0');
+	}
+	else
+	{
+		_putchar((l + '0') * -1);
+	}
 	return (l);
 }
