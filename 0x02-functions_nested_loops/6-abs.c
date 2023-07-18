495bb0b3fa8229a026eabde4e23a496668839cdd
malakehab2003 <lokaehab2003@gmail.com>
2023-07-18 21:41:46 +0000

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * _abs - calculate the abs value for the num
+ *
+ * Return: abs value of the number0
+ *
+ *@c: the nuber gets its abs
+*/
+
+int _abs(int c)
+{
+	return (abs(c));
+}
+
