83e2e5a9a6a2ebcd5e813fc24b549f71a7dd6625
malakehab2003 <lokaehab2003@gmail.com>
2023-07-18 22:30:07 +0000

    print_each_minute

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,10 +12,10 @@ void jack_bauer(void)
 {
 	int i = 0, j = 0;
 
-	while (i < 23)
+	while (i < 24)
 	{
 		j = 0;
-		while (j < 59)
+		while (j < 60)
 		{
 			_putchar((i / 10) + '0');
 			_putchar((i % 10) + '0');
