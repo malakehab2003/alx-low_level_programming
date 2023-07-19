ef568d91313b94e79722fe95337066f011ad774a
malakehab2003 <lokaehab2003@gmail.com>
2023-07-19 01:00:04 +0000

    fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,21 +6,21 @@
  * Return: Always 0 (Success)
 */
 
-
-int main() {
+int main(void)
+{
 	int fib[50] = {1, 2};
 	int i = 2;
-	while (i < 50)
-	{
-		fib[i] = fib[i-1] + fib[i-2];
-		i++;
-	}
 	int j = 0;
-	while (j < 49)
+	while (j < 49) 
 	{
 		printf("%d, ", fib[j]);
 		j++;
 	}
 	printf("%d\n", fib[49]);
-	return (0);
+	while (i < 50)
+	{
+		fib[i] = fib[i-1] + fib[i-2];
+		i++;
+	}
+	return 0;
 }
