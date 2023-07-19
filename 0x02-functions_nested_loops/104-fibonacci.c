1769b37646d4ee66dc960ee04bc7d9459a030c41
malakehab2003 <lokaehab2003@gmail.com>
2023-07-19 01:38:04 +0000

    f

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int count, initia10s;
+	unsigned long f1 = 1, f2 = 2, sum, mx = 100000000, f1o = 0, f2o = 0, sumo = 0;
+
+	for (count = 1; count <= 98; count++)
+	{
+		if (f1o > 0)
+			printf("%lu", f1o);
+		initia10s = numLenth(mx) - 1 - numLength(f1);
+
+		while (f1o > 0 && initia10s > 0)
+		{
+			printf("%d", 0);
+			initia10s--;
+		}
+
+		printf("lu",f1);
+
+		sum = (f1 + f2) % mx;
+		sumo = f1o + f2o +(f1 + f2) / mx;
+		f1 = f2;
+		f1o = f2o;
+		f2 = sum;
+		f2o = sumo;
+
+		if (count != 98)
+			printf(", ");
+		else printf("\n");
+	}
+	return (0);
+}
