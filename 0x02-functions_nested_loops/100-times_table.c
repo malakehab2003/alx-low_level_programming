1717a1ace35555cc079488bce3e312f4a5e656fe
malakehab2003 <lokaehab2003@gmail.com>
2023-07-19 00:02:44 +0000

    print_time_table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -14,21 +14,33 @@ void print_times_table(int n)
 {
         int i = 0, j = 0, m;
 
+	if (n > 15 || n < 0)
+	{
+		return ();
+	}
+	
         while (i <= n)
         {
                 j = 0;
                 while (j <= n)
                 {
                         m = i * j;
-                        if (m > 9)
+                        if (m > 9 && m < 100)
                         {
                                 _putchar((m / 10) + '0');
                                 _putchar((m % 10) + '0');
                         }
-                        else
+                        else if (m < 10)
                         {
                                 _putchar('0' + m);
                         }
+			else
+			{
+				_putchar((m / 100) + '0');
+				_putchar((m / 10) + '0');
+				_putchar((m % 10) + '0');
+			}
+
                         if (j == n)
                         {
                                 _putchar('\n');
@@ -39,6 +51,7 @@ void print_times_table(int n)
                                 {
                                         _putchar(',');
                                         _putchar(' ');
+					_putchar(' ');
                                 }
                         }
                         j++;
