2d7eaea87a39476a1c1cfaa11fe13a0a08bf4e42
malakehab2003 <lokaehab2003@gmail.com>
2023-07-19 00:31:23 +0000

    print_time_table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,6 +10,8 @@
  *@n: the end point
 */
 
+void process(void)
+
 void print_times_table(int n)
 {
 	int i = 0, j = 0, m;
@@ -21,44 +23,49 @@ void print_times_table(int n)
 			j = 0;
 			while (j <= n)
 			{
-				m = i * j;
-				if (m > 9 && m < 100)
-				{
-					_putchar(' ');
-					_putchar((m / 10) + '0');
-					_putchar((m % 10) + '0');
-				}
-				else if (m < 10)
-				{
-					if (j > 0)
-					{
-						_putchar(' ');
-						_putchar(' ');
-					}
- 					_putchar('0' + m);
-				}
-				else
-				{
-					_putchar((m / 100) + '0');
-					_putchar(((m / 10) % 10) + '0');
-					_putchar((m % 10) + '0');
-				}
-
-				if (j == n)
-				{
-					_putchar('\n');
-				}
-				else
-				{
-					if (j != n)
-					{
-						_putchar(',');
-						_putchar(' ');
-					}
-				}
+				process
 				j++;
 			}
 			i++;
 		}	
+	}
+}
+
+void process(void)
+{
+	m = i * j;
+	if (m > 9 && m < 100)
+	{
+		_putchar(' ');
+		_putchar((m / 10) + '0');
+		_putchar((m % 10) + '0');
+	}
+	else if (m < 10)
+	{
+		if (j > 0)
+		{
+			_putchar(' ');
+			_putchar(' ');
+		}
+		_putchar('0' + m);
+	}
+	else
+	{
+		_putchar((m / 100) + '0');
+		_putchar(((m / 10) % 10) + '0');
+		_putchar((m % 10) + '0');
+	}
+
+	if (j == n)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		if (j != n)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
 	}
 }
