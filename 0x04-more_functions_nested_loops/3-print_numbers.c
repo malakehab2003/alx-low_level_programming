b4a0cb38d1c553746c49a8ac76237a440b6f1a80
malakehab2003 <lokaehab2003@gmail.com>
2023-07-20 09:57:42 +0000

    print the numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the first 10 numbers
+ *
+ * Return: no return
+ *
+*/
+
+void print_numbers(void)
+{
+	int i = 48;
+
+	while (i <= 57)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
