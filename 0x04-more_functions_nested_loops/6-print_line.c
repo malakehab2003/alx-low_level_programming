72d98d7ecff54a82b5256b52bbdc578df043f19a
malakehab2003 <lokaehab2003@gmail.com>
2023-07-20 11:17:49 +0000

    print_line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_line - Print a separate line
+ *
+ * Return: no return
+ *
+ *@n: numbers of _ to print
+*/
+
+void print_line(int n)
+{
+	if (n < 1)
+	{
+		_putchar('\n');
+	}
+
+	for (; n >= 0; n--)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
