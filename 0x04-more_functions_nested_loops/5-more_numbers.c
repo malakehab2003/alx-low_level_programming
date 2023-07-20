0caccd4a1d5d581e3bc70f764046eac3ee81cb65
malakehab2003 <lokaehab2003@gmail.com>
2023-07-20 10:21:37 +0000

    more_numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * more_numbers - Entry point
+ *
+ * Return: no return
+*/
+
+void more_numbers(void)
+{
+	int i = 48, j = 48, count = 0;
+
+	while (count < 10)
+	{
+		j = 0;
+		while (i < 49)
+		{
+			if (i < 48)
+			{
+				_putchar(i);
+			}
+			while (j <= 57)
+			{
+				if (i == 49 && j == 52)
+				{
+					break;
+				}
+				_putchar(j);
+				j++;
+			}
+			i++;
+		}
+		if (count != 9)
+		{
+			_putchar('\n');
+		}
+		count++;
+	}
+
+}
