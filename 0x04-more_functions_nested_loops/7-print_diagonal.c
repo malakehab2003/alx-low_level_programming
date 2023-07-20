5e2dc6054cfb688cc3db8cd8337d4f776264f4a8
malakehab2003 <lokaehab2003@gmail.com>
2023-07-20 11:48:03 +0000

    print_diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print a diagonal
+ *
+ * Return: no return
+ *
+ * @n: number of the diagonal
+*/
+
+void print_diagonal(int n)
+{
+	int i = 0, j = n;
+
+	for (; n > 0; n--)
+	{
+		i = 0;
+		for (; i < j - n; i++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
--- 0x04-more_functions_nested_loops/main.c
+++ 0x04-more_functions_nested_loops/main.c
@@ -2,8 +2,9 @@
 #include "5-more_numbers.c"
 #include "_putchar.c"
 #include "6-print_line.c"
+#include "7-print_diagonal.c"
 
 int main(void)
 {
-	print_line(-1);
+	print_diagonal(3);
 }
