b42b9425853de8e5a4a8f66e2c649a51b810072b
malakehab2003 <lokaehab2003@gmail.com>
2023-07-24 19:38:03 +0000

    swap

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse a string
+ *
+ * Return: void
+ *
+ * @s: the string to reverse
+*/
+
+void rev_string(char *s)
+{
+	int i = 0, c = _strlen(s) - 1, p;
+
+	while (i <= c / 2)
+	{
+		p = *(s + c);
+		*(s + i) = *(s + c);
+		*(s + c) = p;
+		i++;
+		c--;
+	}
+}
