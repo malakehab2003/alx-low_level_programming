4b0ea3f1161fad01f880bfe1c52f009ad5325f54
malakehab2003 <lokaehab2003@gmail.com>
2023-07-24 20:45:35 +0000

    puts_half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * puts_half - Entry point
+ *
+ * Return: void
+ *
+ *@str: 
+*/
+
+void puts_half(char *str)
+{
+	int i = _strlen(str);
+
+	if (i % 2== 0)
+	{
+		i /=2;
+		while(str[i] != '\0')
+		{
+			_putchar(str[i]);
+			i++;
+		}
+	}
+	else
+	{
+		i = (i - 1) / 2;
+		while(str[i] != '\0')
+		{
+			_putchar(str[i]);
+			i++;
+		}
+	}
+}
