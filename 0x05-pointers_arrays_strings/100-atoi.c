95f7a87ccfa6721f061eba73752ef99ad9fdb0a2
malakehab2003 <lokaehab2003@gmail.com>
2023-07-24 22:32:16 +0000

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,30 +10,24 @@
 
 int _atoi(char *s)
 {
-	int i = 0, j = 48;
+	int i = 0, j = 48, count = 0;
 
 	while (s[i] != '\0')
 	{
-		if ('s[i]' == 45)
-		{
-			s[i] = -;
-		}
-		else if (s[i] >= 48 && s[i] <= 57)
+		if (s[i] >= 48 && s[i] <= 57)
 		{
 			while (j <= 57)
 			{
-				if ('s[i]' == j)
+				if (s[i] == j)
 				{
-					s[i] = j;
+					count += j;
 				}
+				count *= 10;
 				j++;
 			}
 		}
-		else
-		{
-			i++;
-			continue;
-		}
 		i++;
 	}
+	return (count);
+
 }
