a95dbd8a2254700a62ea8a7fd09be0a68360ab91
malakehab2003 <lokaehab2003@gmail.com>
2023-07-26 14:01:18 +0000

    More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ *
+ * Return: capitalized string
+ *
+ * @s: the string to capitalize
+*/
+
+char *cap_string(char *s)
+{
+	for (int i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == ' ' || s[i] == '\t' || s[i] == '\n' || s[i] == ',' || s[i] == ';' || s[i] == '.' || s[i] == '!' || s[i] == '?' || s[i] == '"' || s[i] == '(' || s[i] == ')' || s[i] == '{' || s[i] == '}' || i == 0)
+		{
+			i++;
+			if (s[i] >= 97 && s[i] <= 122)
+			{
+				s[i] -= 97;
+				s[i] += 65;
+			}
+			else
+			{
+				continue;
+			}
+		}
+		else
+		{
+			i++;
+			continue;
+		}
+	}
+	return (s);
+}
