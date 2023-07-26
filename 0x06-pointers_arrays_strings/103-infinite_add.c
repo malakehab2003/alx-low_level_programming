b2f2ef69a5b40343c487e2a8ae7a2cbc7e0893c3
malakehab2003 <lokaehab2003@gmail.com>
2023-07-26 20:27:23 +0000

    More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include "atoi.c"
+
+/**
+ * infinite_add - add numbers
+ *
+ * Return: the number
+*/
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int no1 = _atoi(n1), no2 = _atoi(n2), result, count = 0, cp, resultcp;
+
+	result = no1 + no2;
+	resultcp = result
+	
+	while (result > 0)
+	{
+		count++;
+		result /= 10;
+	}
+	
+	if (count >= size_r)
+	{
+		return (0);
+	}
+
+	count--;
+	while (resultcp > 0)
+	{
+		cp = resultcp;
+		r[count] = 'cp % 10';
+		result /= 10;
+		count--;
+	}
+}
