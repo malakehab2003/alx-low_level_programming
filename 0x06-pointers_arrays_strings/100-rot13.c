b6317f508b30505d9bd3abcc0fa85b6dddc4095a
malakehab2003 <lokaehab2003@gmail.com>
2023-07-26 17:41:27 +0000

    More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes the alphabet
+ *
+ * Return: encoded text
+ *
+ * @s: the string
+*/
+
+char *rot13(char *s)
+{
+	int i = 0, j = 0;
+	char b[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char a[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (; s[i] != '\0'; i++)
+	{
+		for (; j < (26 * 2); j++)
+		{
+			if (s[i] == b[j])
+			{
+				s[i] = a[j];
+			}
+		}
+	}
+}
