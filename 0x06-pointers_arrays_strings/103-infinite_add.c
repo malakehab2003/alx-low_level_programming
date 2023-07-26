f98f3344a031138c9e1d0ea65ccb54512a802983
malakehab2003 <lokaehab2003@gmail.com>
2023-07-26 20:58:18 +0000

    More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -35,3 +35,39 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	}
 	return (r);
 }
+
+/**
+ * _atoi - change int into string
+ *
+ * Return: the intger in the string
+ *
+ *@s: the string to convert
+*/
+
+int _atoi(char *s)
+{
+	int i = 0, sign = 1;
+	unsigned int count = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-' && count == 0)
+		{
+			sign *= -1;
+		}
+		else if (s[i] == '+' && count == 0)
+		{
+			sign *= 1;
+		}
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			count = count * 10 + (s[i] - '0');
+		}
+		else if (count > 0)
+		{
+			break;
+		}
+		i++;
+	}
+	return (count * sign);
+}
