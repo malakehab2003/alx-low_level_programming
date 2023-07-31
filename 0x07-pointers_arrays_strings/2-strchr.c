96c51830f013f91bf5723ef72ba44807a3a25d55
malakehab2003 <lokaehab2003@gmail.com>
2023-07-31 12:04:40 +0000

    Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -14,12 +14,13 @@ char *_strchr(char *s, char c)
 {
 	unsigned int i = 0;
 
-	for (; s[i] != '\0'; i++)
+	while (s[i] != '\0')
 	{
 		if (s[i] == c)
 		{
 			return (&s[i]);
 		}
+		i++;
 	}
 	return (NULL);
 }
