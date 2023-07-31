c82838b914f7b83b71ca3cf30cd33a9885dbab25
malakehab2003 <lokaehab2003@gmail.com>
2023-07-31 18:35:03 +0000

    Even more pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * set_string - make the pointer to a pointer
+ *
+ * Return: void
+ *
+ * @s: the pointer to set
+ *
+ * @to: the pointer to set to
+*/
+
+void set_string(char **s, char *to)
+{
+	s = &to;
+}
