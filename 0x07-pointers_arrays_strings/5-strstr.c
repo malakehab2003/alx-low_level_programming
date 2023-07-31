a03e6fbca2f256152f205961a3e19fe086c0e9a3
malakehab2003 <lokaehab2003@gmail.com>
2023-07-31 15:53:40 +0000

    Even more pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0, j = 0, count = 0, count2 = 0, k = 0;
+
+	for (needle[k] != '\0', k++)
+	{
+		count++;
+	}
+
+	for (; haystack[i] != '\0'; i++)
+	{
+		if (haystack[i] == needle[j])
+		{
+			j = 0;
+			for (; needle[j] != '\0'; j++)
+			{
+				if (haystack[i + j] == needle[j])
+				{
+					count2++;
+				}
+			}
+			if (count == count2)
+			{
+				return (haystack[i]);
+			}
+		}
+	}
+	return (0);
+}
