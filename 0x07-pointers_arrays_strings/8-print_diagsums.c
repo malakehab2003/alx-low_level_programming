20983bc751a0594a892ac891a1a463c8a65b2f8c
malakehab2003 <lokaehab2003@gmail.com>
2023-07-31 16:47:41 +0000

    Even more pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_diagsums - Entry point
+ *
+ * Return: void
+ *
+ * @a: the array to count
+ *
+ * @size: the size of the array
+*/
+
+void print_diagsums(int *a, int size)
+{
+	int i, j, count = 0;
+
+	for (i = 0, j = 0; i < size && j < size; i++, j++)
+	{
+		count += a[i][j];
+	}
+
+	_putchar(count);
+	_putchar(',');
+	_putchar(' ');
+	for (i = size, j = size; i < 0 && j < 0; i--, j--)
+	{
+		count += a[i][j];
+	}
+	_putchar(count);
+}
