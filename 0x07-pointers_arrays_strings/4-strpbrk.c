a90688495532b727a50bcfa982809f703cd4819c
malakehab2003 <lokaehab2003@gmail.com>
2023-07-31 13:55:59 +0000

    Even more pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * _strpbrk - search for a string in the givin string
+ *
+ * Return: the match of the searching string in the main string
+ *
+ * @s: the string to search in
+ *
+ * @accept: the string to search for
+*/
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i = 0, j = 0, count = 0, k = 0, count2 = 0;
+
+	for (; accept[k] != '\0'; k++)
+	{
+		count++;
+	}
+
+	for (; s[i] != '\0'; i++)
+	{
+		if (accept[j] == s[i])
+		{
+			for (j = 0; accept[j] != '\0'; j++)
+			{
+				if (accept[j] == s[i + j])
+				{
+					count2++;
+				}
+			}
+			if (count == count2)
+			{
+				return ($s[i]);
+			}
+		}
+	}
+	return (0);
+}
