5b7b7213b5b2d2fae53924db49c45f3720211cdc
malakehab2003 <lokaehab2003@gmail.com>
2023-08-02 12:11:55 +0000

    Recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - print reverse string
+ *
+ * Return: void
+ *
+ * @s: the stirng to print
+*/
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return ();
+	}
+	else
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
