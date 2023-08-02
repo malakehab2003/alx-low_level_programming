17a7185a1d09603a1899bf16526a0e2777d778b9
malakehab2003 <lokaehab2003@gmail.com>
2023-08-02 11:50:35 +0000

    Recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - print a string
+ *
+ * Return: void
+ *
+ * @s: the string to print
+*/
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		_putchar('\0');
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(*(s + 1)); 
+	}
+}
