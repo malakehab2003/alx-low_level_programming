00b541882cdd869dcab11f1472e7c44874e966e3
malakehab2003 <lokaehab2003@gmail.com>
2023-08-02 17:38:00 +0000

    Recursion

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -8,13 +8,16 @@
 
 int wildcmp(char *s1, char *s2)
 {
-	if (*s1 == 0&& *s2 == 0)
+	if (*s1 == 0 && *s2 == 0)
 	{
 		return (1);
 	}
 	else if (*s2 == '*')
 	{
-		s2++;
+		if (s2++ == '*')
+		{
+			wildcmp(s1, s2++);
+		}
 		if (*s1 != *s2)
 		{
 			return (wildcmp(s1 + 1, s2));
