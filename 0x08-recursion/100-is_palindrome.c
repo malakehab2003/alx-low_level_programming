e7672f5ffd5c349dfefabd689408f3f92e23b7f6
malakehab2003 <lokaehab2003@gmail.com>
2023-08-02 16:21:39 +0000

    Recursion

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * is_palindrome - check if the string is empty
+ *
+ * Return: 1 if empty
+ *
+ * @s: the string
+*/
+
+int is_palindrome(char *s)
+{
+	if (* == 0)
+		return (1);
+	else
+		return (0);
+}
