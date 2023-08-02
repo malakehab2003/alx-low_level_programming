714563ccef0fa2d6c444fd3c5d8ce4a50cde267d
malakehab2003 <lokaehab2003@gmail.com>
2023-08-02 17:26:11 +0000

    Recursion

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == 0&& *s2 == 0)
+	{
+		return (1);
+	}
+	else if (*s2 == '*')
+	{
+		s2++;
+		if (*s1 != *s2)
+		{
+			return (wildcmp(s1 + 1, s2);
+		}
+	}
+	return (wildcmp(s1 + 1, s2 + 1));
+}
