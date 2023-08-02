029f1dc10e28600a1c9fa7799ce16a47f60c655d
malakehab2003 <lokaehab2003@gmail.com>
2023-08-02 18:15:42 +0000

    Recursion

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,31 +1,31 @@
 #include "main.h"
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
-*/
+ * wildcmp - Compare strings
+ * @s1: first string
+ * @s2: second string
+ * Return: 1 if equal
+ */
 
 int wildcmp(char *s1, char *s2)
 {
-	if (*s1 == 0 && *s2 == 0)
+	if (*s1 == '\0')
 	{
-		return (1);
-	}
-	else if (*s2 == '*')
-	{
-		if (*s2 == '*')
+		if (*s2 != '\0' && *s2 == '*')
 		{
-			wildcmp(s1, s2++);
+			return (wildcmp(s1, s2 + 1));
 		}
-		if (*s1 != *s2)
-		{
-			return (wildcmp(s1 + 1, s2));
-		}
+		return (*s2 == '\0');
 	}
-	else if (*s1 != *s2)
+
+	if (*s2 == '*')
 	{
-		return (0);
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
 	}
-	return (wildcmp(s1 + 1, s2 + 1));
+	else if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	return (0);
 }
+
