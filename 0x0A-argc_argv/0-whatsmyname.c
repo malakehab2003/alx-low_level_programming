0958cef672770812bb6210994b214ae9239a490e
malakehab2003 <lokaehab2003@gmail.com>
2023-08-04 15:41:14 +0000

    argc, argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,10 +9,6 @@
 
 int main(int argc __attribute__((unused)), char *argv[])
 {
-	int i;
-
-	for (i = 0; argv[0][i] != '\0'; i++)
-	{
-		printf("%s", argv[0][i]);
-	}
+	printf("%s", *argv);
+	return (0);
 }
