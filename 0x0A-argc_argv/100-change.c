dbee8e98e08dd06149d9cce3375f794a26b0deef
malakehab2003 <lokaehab2003@gmail.com>
2023-08-04 19:24:03 +0000

    argc, argv

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ * @argc: the number of the arguments
+ *
+ * @argv: the arguments
+*/
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	int cents = atoi(argv[1]), count = 0;
+
+	if (cents < 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	while (cents >= 25)
+        {
+                count++;
+                cents -= 25;
+        }
+
+	while (cents >= 10)
+        {
+                count++;
+                cents -= 10;
+        }
+        
+	while (cents >= 5)
+        {
+                count++;
+                cents -= 5;
+        }
+
+	while (cents >= 2)
+        {
+                count++;
+                cents -= 2;
+        }
+
+	while (cents >= 1)
+	{
+		count++;
+		cents -= 1;
+	}
+
+	printf("%d", count);
+
+	return (0);
+}
