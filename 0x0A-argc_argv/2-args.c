1e34010485816a3ce10903dbe8d589b4479c2f72
malakehab2003 <lokaehab2003@gmail.com>
2023-08-04 17:01:05 +0000

    argc, argv

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -16,8 +16,10 @@ int main(int argc, char *argv[])
 {
 	int i;
 
-	for (i = 0; i < argc - 1; i++)
+	for (i = 0; i < argc; i++)
 	{
-		printf("%s", argv[i]);
+		printf("%s\n", argv[i]);
 	}
+
+	return (0)
 }
