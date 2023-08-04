298b0f3be78e03283a3a5a87ad5f684b773a7021
malakehab2003 <lokaehab2003@gmail.com>
2023-08-04 15:42:46 +0000

    argc, argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,10 +2,13 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - prints the name of the program
+ * @argc: number of arguments
+ * @argv: array of arguments
  *
  * Return: Always 0 (Success)
 */
+
 
 int main(int argc __attribute__((unused)), char *argv[])
 {
