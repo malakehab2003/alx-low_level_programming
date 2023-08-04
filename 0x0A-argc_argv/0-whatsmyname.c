bfd068e5f121cdb820ac7476f1174dda05e4c8c8
malakehab2003 <lokaehab2003@gmail.com>
2023-08-04 15:28:53 +0000

    argc, argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,10 +8,19 @@
 
 int main(int argc, char *argv[])
 {
-	int i;
+	int i, j;
+	char slach = '/';
+	char *name = argv[0];
 
+	for (j = 0; argv[0][j] != '\0'; j++)
+	{
+		if (argv[0][j] == slach)
+		{
+			name = argv[0][j] + 1;
+		}
+	}
 	for (i = 0; argv[0][i] != '\0'; i++)
 	{
-		_putchar(argv[0][i]);
+		_putchar(*name);
 	}
 }
