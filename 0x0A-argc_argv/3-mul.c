e26337664a551c2bf90ac28db7da396c18143be4
malakehab2003 <lokaehab2003@gmail.com>
2023-08-04 17:23:13 +0000

    argc, argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <unistd.h>
 #include "main.h"
-#include "atoi.c"
+
+int _atoi(char *s);
 
 /**
  * main - Entry point
@@ -23,4 +24,40 @@ int main(int argc, char *argv[])
 	printf("%d\n", first);
 
 	return (0);
+}
+
+/**
+ * _atoi - change int into string
+ *
+ * Return: the intger in the string
+ *
+ *@s: the string to convert
+*/
+
+int _atoi(char *s)
+{
+	int i = 0, sign = 1;
+	unsigned int count = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-' && count == 0)
+		{
+			sign *= -1;
+		}
+		else if (s[i] == '+' && count == 0)
+		{
+			sign *= 1;
+		}
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			count = count * 10 + (s[i] - '0');
+		}
+		else if (count > 0)
+		{
+			break;
+		}
+		i++;
+	}
+	return (count * sign);
 }
