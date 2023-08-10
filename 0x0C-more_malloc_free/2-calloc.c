9ea8ae0a4a71a32b3d00fa0563a6e63cd4f367f4
malakehab2003 <lokaehab2003@gmail.com>
2023-08-10 18:58:19 +0000

    :more_malloc_free

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - allocate an array
+ *
+ * Return: pointer to an array
+ *
+ * @nmemb: the array to allocate
+ *
+ * @size: the size of the array
+*/
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int i = 0;
+	char *arr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	arr = malloc(nmemb * size);
+	if (arr == NULL)
+		return (NULL);
+	while (i < nmemb)
+	{
+		arr[i] = '0';
+		i++;
+	}
+	return (arr);
+}
