c70f60ef212a49be1141f44b3b63ed37e4dcb99f
malakehab2003 <lokaehab2003@gmail.com>
2023-08-10 20:16:26 +0000

    :more_malloc_free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -27,7 +27,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		free(ptr);
 		return (NULL);
 	}
-	if (ptr != NULL)
+	if (ptr == NULL)
 	{
 		arr = malloc(new_size);
 		if (arr == NULL)
