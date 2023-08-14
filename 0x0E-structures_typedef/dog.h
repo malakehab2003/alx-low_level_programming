533a5900a38b755cc9e887dd6284870a7997f358
malakehab2003 <lokaehab2003@gmail.com>
2023-08-14 19:07:04 +0000

    structures_typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,12 @@
+#ifndef DOG_H
+#define DOG_H
+
 /**
  * dog - dog struct
+ *
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner
 */
 
 struct dog
@@ -8,3 +15,6 @@ struct dog
 	float age;
 	char *owner;
 } dog;
+
+
+#endif
