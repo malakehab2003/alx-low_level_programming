d6e338cfe01596adebb5c1c34ef39dd5c07d3195
malakehab2003 <lokaehab2003@gmail.com>
2023-08-16 20:40:56 +0000

    function_pointers

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -17,13 +17,20 @@
 
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	int n1 = atoi(argv[1]),  n2 = atoi(argv[3]);
+	int num1, num2;
+	char *op;
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
+
+	num1 = atoi(argv[1]);
+	op = argv[2];
+	num2 = atoi(argv[3]);
+
+
 	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
