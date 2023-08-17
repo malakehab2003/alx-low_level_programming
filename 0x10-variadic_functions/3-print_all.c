d75edf553fa9f017c6a28ec036fee0f1f442b0fa
malakehab2003 <lokaehab2003@gmail.com>
2023-08-17 19:18:12 +0000

    variadic_functions

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -12,13 +12,9 @@ void print_all(const char * const format, ...)
 	int i = 0;
 	char *str, *sep = "";
 
-
 	va_list list;
 
-
 	va_start(list, format);
-
-
 	if (format)
 	{
 		while (format[i])
@@ -48,9 +44,6 @@ void print_all(const char * const format, ...)
 			i++;
 		}
 	}
-
-
 	printf("\n");
 	va_end(list);
-
 }
