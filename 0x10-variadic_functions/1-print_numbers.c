bc03a52e42a03488d50b5f3e3c987208bb192630
malakehab2003 <lokaehab2003@gmail.com>
2023-08-17 17:19:47 +0000

    variadic_functions

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -33,7 +33,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		for (; i < n; i++)
 		{
-			printf("%d%s", va_arg(args, int), separator);
+			if (i != n - 1)
+			{
+				printf("%d%s", va_arg(args, int), separator);
+			}
 		}
 	}
 	printf("\n");
