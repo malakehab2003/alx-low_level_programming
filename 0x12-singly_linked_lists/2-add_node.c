3b8d7ec93fabef8caaeecf08c8f244d0b8132aaf
malakehab2003 <lokaehab2003@gmail.com>
2023-08-24 16:04:11 +0000

    0x12-singly_linked_lists

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -15,9 +15,6 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new;
 	int i = 0;
 
-	if (str == NULL)
-		return (NULL);
-
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
