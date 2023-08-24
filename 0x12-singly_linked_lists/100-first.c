3e5da97b5a2dfc8c72b984e9823cfb2b88adb1e9
malakehab2003 <lokaehab2003@gmail.com>
2023-08-24 19:24:37 +0000

    0x12-singly_linked_lists

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- * function - function print before main
+ * first - function print before main
  *
  * Return: void
 */
 
-void function(void)__attribute__((constructor));
+void first(void)__attribute__((constructor));
 
-void function(void)
+void first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
