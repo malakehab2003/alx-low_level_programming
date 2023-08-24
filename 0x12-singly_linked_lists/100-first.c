c9fa7c40cdd6dc4ed213fb1c8b19d661f9513e31
malakehab2003 <lokaehab2003@gmail.com>
2023-08-24 19:21:01 +0000

    0x12-singly_linked_lists

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
+
 /**
  * function_run_before_main - function print before main
  *
  * Return: void
 */
 
-void function_run_before_main(void) __attribute__((constructor));
+void function_run_before_main(void)__attribute__((constructor));
 
 void function_run_before_main(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
