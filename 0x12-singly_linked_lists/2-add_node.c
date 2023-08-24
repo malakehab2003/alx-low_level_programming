0247253433b9f462593632158712577440f8854e
malakehab2003 <lokaehab2003@gmail.com>
2023-08-24 16:04:48 +0000

    0x12-singly_linked_lists

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -20,11 +20,6 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	new->str = strdup(str);
 
-	if (new->str == NULL)
-	{
-		free(new);
-		return (NULL);
-	}
 	while (str[i] != 0)
 	{
 		i++;
