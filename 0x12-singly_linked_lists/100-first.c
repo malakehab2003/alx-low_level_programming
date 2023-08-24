ae1ab47da11fc7b17d560ebb2611f4d76c1934c1
malakehab2003 <lokaehab2003@gmail.com>
2023-08-24 19:14:05 +0000

    0x12-singly_linked_lists

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+
+/**
+ * function_run_before_main - function print before main
+ *
+ * Return: void
+*/
+
+void function_run_before_main(void) __attribute__((constructor));
+
+void function_run_before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
