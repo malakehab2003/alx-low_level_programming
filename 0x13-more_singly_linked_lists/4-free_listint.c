b320f0329f0c2727938fc48e95f8921d7f723663
malakehab2003 <lokaehab2003@gmail.com>
2023-08-28 07:56:04 -0700

    free_listint

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_list - free linked list
+ *
+ * Return: void
+ *
+ * @head: pointer to the first pointer
+ *
+*/
+
+void free_listint(listint_t *head)
+{
+	listint_t *new;
+
+	while (head != NULL)
+	{
+		new = head->next;
+		free(head);
+		head = tmep;
+	}
+}
