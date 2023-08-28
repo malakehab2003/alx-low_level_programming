07bb01a357cf858b8e66982ba1cd00e90b09d95e
malakehab2003 <lokaehab2003@gmail.com>
2023-08-28 06:02:48 -0700

    more_singly_linked_lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,5 +1,13 @@
 #include "lists.h"
 
+/**
+ * print_listint - print linked list
+ *
+ * Return: size of the list
+ *
+ * @h: the pointer to the list
+*/
+
 size_t print_listint(const listint_t *h)
 {
 	listint_t *new = h;
