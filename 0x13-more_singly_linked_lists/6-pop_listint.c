2cff619060ad0d8901596164d20f99f4201a3a1f
malakehab2003 <lokaehab2003@gmail.com>
2023-08-28 10:06:23 -0700

    pop_listint

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -14,7 +14,7 @@ int pop_listint(listint_t **head)
 	int n = 0;
 	listint_t *temp = (*head)->next;
 
-	if (*head == NULL)
+	if (*head == NULL || head == NULL)
 		return (0);
 	n = (*head)->n;
 	free(*head);
