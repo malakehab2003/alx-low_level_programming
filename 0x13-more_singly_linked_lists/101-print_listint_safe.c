35255d9f4f545ab65c371d3332af40c22adbeafe
malakehab2003 <lokaehab2003@gmail.com>
2023-08-29 06:41:25 -0700

    not finished

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - print list
+ *
+ * Return: number of node
+ *
+ * @head: pointer to the first pointer
+ *
+*/
+
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t count = 0;
+	listint_t *new;
+
+
+}
