1827fab2da0da75c2cb060f01e0a25e5be6617ea
malakehab2003 <lokaehab2003@gmail.com>
2023-08-29 03:51:01 -0700

    delete_nodeint_at_index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -17,6 +17,8 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	unsigned int count = 0;
 
 	tmp = *head;
+	if (tmp == NULL)
+		return (-1);
 	if (index == 0)
 	{
 		*head = (*head)->next;
