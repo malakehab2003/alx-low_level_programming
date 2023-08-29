dcd77d5ff8ed9f42ef90a2922807d2706be8c9fc
malakehab2003 <lokaehab2003@gmail.com>
2023-08-29 11:16:28 -0700

    final task

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop - find loop in linked list
+ *
+ * Return: pointer to a loop
+ *
+ * @head: pointer to the first pointer
+ *
+*/
+
+listint_t *find_listint_loop(listint_t *head)
+{
+	int min;
+
+	if (head == NULL)
+		return (NULL);
+	while (head != NULL)
+	{
+		min = h - h->next;
+		if (min > 0)
+		{
+			return (h);
+		}
+	}
+	return (NULL);
+}
