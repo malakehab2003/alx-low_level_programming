a3cc382ced581ddaa65d46a24bcd6ff7d4da90a2
malakehab2003 <lokaehab2003@gmail.com>
2023-09-04 11:56:16 -0700

    last time using sandbox

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * print_list - print linked list
+ *
+ * Return: the 
+ *
+ * @filename: name of the file
+*/
+
+int create_file(const char *filename, char *text_content)
