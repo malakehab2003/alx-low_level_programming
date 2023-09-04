212b326f29c278f4b5c1f1eb259dadaa0da11f7a
malakehab2003 <lokaehab2003@gmail.com>
2023-09-04 12:35:11 -0700

    second task

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,11 +1,40 @@
 #include "main.h"
 
 /**
- * print_list - print linked list
+ * create_file - create a file
  *
- * Return: the 
+ * Return: 1 if success
  *
  * @filename: name of the file
+ * 
+ * @text_content: the content of the file
 */
 
 int create_file(const char *filename, char *text_content)
+{
+        ssize_t file;
+        int len = 0, filew;
+
+        if (filename == NULL)
+                return (-1);
+        file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+        if (file == -1)
+                return (-1);
+        if (text_content != NULL)
+        {
+                while (text_content != 0)
+                {
+                        len++;
+                }
+        }
+
+        filew = write(file, text_content, len)
+
+        if (filew == NULL)
+                return (-1);
+
+        close(file);
+        return (1);
+
+
+}
