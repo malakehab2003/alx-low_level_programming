c961ad4a72ff0a38efa2cdd89ff83597b26ed4fb
malakehab2003 <lokaehab2003@gmail.com>
2023-09-04 10:54:26 -0700

    first task

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdio.h>
+
+/**
+ * read_textfile - read textfile
+ *
+ * Return: size of the list
+ *
+ * @letters: the number of the letters to read
+ *
+ * @filename: the name of the file to read
+*/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buffer;
+	ssize_t file, filer, filew;
+
+	if (filename == NULL)
+		return (0);
+	file = fopen("filename", O_RDONLY);
+	if (file = -1)
+		return (0);
+	buffer = malloc(sizeof(char) * (letters + 1));
+	if (buffer == NULL)
+	{
+		free(buffer);
+		fclose(file);
+		return (0);
+	}
+	filer = fread(buffer, 1, letters, file);
+	filew = write(STDOUT_FILENO, buffer, filer);
+
+	free(buffer);
+	fclose(file);
+	return (filew);
+
+}
