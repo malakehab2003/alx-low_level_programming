960bd7418271bbcf3b82c92e243412e97cef4913
malakehab2003 <lokaehab2003@gmail.com>
2023-09-04 12:44:26 -0700

    second task

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,29 +12,27 @@
 
 int create_file(const char *filename, char *text_content)
 {
-        ssize_t file;
-        int len = 0, filew;
+	ssize_t file;
+	int len = 0, filew;
 
-        if (filename == NULL)
-                return (-1);
-        file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-        if (file == -1)
-                return (-1);
-        if (text_content != NULL)
-        {
-                while (text_content[len] != 0)
-                {
-                        len++;
-                }
-        }
+	if (filename == NULL)
+		return (-1);
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (file == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		while (text_content[len] != 0)
+		{
+			len++;
+		}
+	}
 
-        filew = write(file, text_content, len);
+	filew = write(file, text_content, len);
 
-        if (filew == -1)
-                return (-1);
+	if (filew == -1)
+		return (-1);
 
-        close(file);
-        return (1);
-
-
+	close(file);
+	return (1);
 }
