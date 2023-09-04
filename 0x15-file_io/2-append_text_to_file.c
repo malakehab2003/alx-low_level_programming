46825b10265de4963802007a3be1beaa5d5d72d8
malakehab2003 <lokaehab2003@gmail.com>
2023-09-04 13:12:45 -0700

    third

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * append_text - append text
+ *
+ * Return: 1 if success
+ *
+ * @filename: name of the file
+ * 
+ * @text_content: the content of the file
+*/
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	ssize_t file, filew;
+	int len = 0;
+
+	if (filename == NULL)
+		return (-1);
+	file = open(filename, O_APPEND | O_WRONLY);
+	if (file == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		while (text_content[len] != 0)
+			len++;
+	}
+	filew = write(file, text_content, len);
+	if (filew == -1)
+		return (-1);
+	close(file);
+	return (filew);
+}
