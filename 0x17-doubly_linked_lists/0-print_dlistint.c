4fc3b749bef9a91b2f14b2f598c4f61e161434d2
malakehab2003 <lokaehab2003@gmail.com>
2023-10-12 17:54:16 +0300

    finish task  0

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - print data for double linked list
+ *
+ * Return: size of the list
+ *
+ * @h: pointer to the head of the list
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+        const dlistint_t *new;
+        size_t count = 0;
+
+        new = h;
+        while (new != NULL)
+        {
+                printf("%d\n", new->n);
+                count++;
+                new = new->next;
+        }
+        return (count);
+}
+
