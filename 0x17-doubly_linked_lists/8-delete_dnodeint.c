ed456e0f037fd00bf3c87d873ba6c39a7f975ea5
malakehab2003 <lokaehab2003@gmail.com>
2023-10-12 22:16:09 +0300

    finish task  8

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -16,11 +16,19 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 
 	node = *head;
 	if (*head == NULL)
+	{
 		return (-1);
+	}
 	if (index == 0)
 	{
-		*head = (*head)->next;
-		free((*head)->prev);
+		if ((*head)->next != NULL)
+		{
+			*head = (*head)->next;
+			free((*head)->prev);
+			return (1);
+		}
+		free(*head);
+		*head = NULL;
 		return (1);
 	}
 	while (index != 0 && node != NULL)
