6be9dca6dedcfa624dabea3639145a17febdcfce
malakehab2003 <lokaehab2003@gmail.com>
2023-10-12 22:42:10 +0300

    finish task  8

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -12,39 +12,43 @@
 
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *fast, *slow, *node;
+	dlistint_t *h1;
+	dlistint_t *h2;
+	unsigned int i;
 
-	node = *head;
-	if (*head == NULL)
+	h1 = *head;
+
+	if (h1 != NULL)
+		while (h1->prev != NULL)
+			h1 = h1->prev;
+
+	i = 0;
+
+	while (h1 != NULL)
 	{
-		return (-1);
-	}
-	if (index == 0)
-	{
-		if ((*head)->next != NULL)
+		if (i == index)
 		{
-			*head = (*head)->next;
-			free((*head)->prev);
+			if (i == 0)
+			{
+				*head = h1->next;
+				if (*head != NULL)
+					(*head)->prev = NULL;
+			}
+			else
+			{
+				h2->next = h1->next;
+
+				if (h1->next != NULL)
+					h1->next->prev = h2;
+			}
+
+			free(h1);
 			return (1);
 		}
-		free(*head);
-		*head = NULL;
-		return (1);
+		h2 = h1;
+		h1 = h1->next;
+		i++;
 	}
-	while (index != 0 && node != NULL)
-	{
-		index--;
-		node = node->next;
-	}
-	if (node == NULL)
-		return (-1);
-	fast = node->next;
-	slow = node->prev;
-	if (fast != NULL)
-		fast->prev = slow;
-	if (slow != NULL)
-		slow->next = fast;
-	free(node);
-	node = NULL;
-	return (1);
+
+	return (-1);
 }
