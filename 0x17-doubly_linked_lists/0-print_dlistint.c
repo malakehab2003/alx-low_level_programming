47dea236701c8affaa95aab908e9b974a2444ad3
malakehab2003 <lokaehab2003@gmail.com>
2023-10-12 17:58:48 +0300

    finish task  0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -10,16 +10,15 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-        const dlistint_t *new;
-        size_t count = 0;
+	const dlistint_t *new;
+	size_t count = 0;
 
-        new = h;
-        while (new != NULL)
-        {
-                printf("%d\n", new->n);
-                count++;
-                new = new->next;
-        }
-        return (count);
+	new = h;
+	while (new != NULL)
+	{
+		printf("%d\n", new->n);
+		count++;
+		new = new->next;
+	}
+	return (count);
 }
-
