aa6d1614b40dc871cd5d5bcfe4f82757122cb667
malakehab2003 <lokaehab2003@gmail.com>
2023-10-12 19:12:59 +0300

    finish task  4

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -13,21 +13,12 @@
 
 void free_dlistint(dlistint_t *head)
 {
-	dlistint_t *new;
+	dlistint_t *temp;
 
-	if (head != NULL)
+	while (head != NULL)
 	{
-		if (head->next == NULL)
-			free(head);
-		else
-		{
-			new = head->next;
-			while (head != NULL)
-			{
-				free(head);
-				head = new;
-				new = new->next;
-			}
-		}
+		temp = head->next;
+		free(head);
+		head = temp;
 	}
 }
