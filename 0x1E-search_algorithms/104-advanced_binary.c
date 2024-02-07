3d981d2d697a65628b9880ef31621ec2afe9d160
malakehab2003 <lokaehab2003@gmail.com>
2024-02-07 19:58:21 +0200

    advanced binary search

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -48,7 +48,7 @@ int binary_search_recursion(int *array, size_t start, size_t end, int value)
 	if (end < start)
 		return (-1);
 	print_array(array, start, end);
-	if (array[i] == value && array[i - 1] != value)
+	if (array[i] == value && array[i - 1] != value && array[i - 2] != value)
 		return (i);
 	else if (array[i] == value)
 		return (binary_search_recursion(array, start, i, value));
